2ee146529abdeebe03e197a2b939af2fdb1fe766
Aritra Basu <aritrabasu1999@gmail.com>
2018-05-07 20:53:21 +0530

    only post methods

--- Bot.py
+++ Bot.py
@@ -4,34 +4,31 @@ from helpers import *
 app = Flask(__name__)
 bot_id = os.environ['BOT_ID']
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST'])
 def game():
-	if request.method == 'POST':
-		req = request.get_json(silent = True, force = True)
-		message = req.get('message')
-		print()
-		session['id'] = message['from']['id']
-		if message['text'] == 'New game':
-			(status, session['word'], session['current_word'], session['tries']) = new_game()
-			if not status:
-				reply(bot_id, 'Failed to start new game', session['id'])
-			else:
-				session['game started'] = True
-				reply(bot_id, "Enter a character", session['id'])
-		elif message['text'] == 'Cancel' and session['game started']:
-			session.clear()
-		elif session['game started']:
-			(status, current_word) = guess(session['word'], session['current_word'], session['tries'], message['text'])
-			if status:
-				reply(bot_id, 'perfect \nCurrent word is'+''.join(current_word), session['id'])
-				reply(bot_id, 'Enter a character', session['id'])
+	req = request.get_json(silent = True, force = True)
+	message = req.get('message')
+	print()
+	session['id'] = message['from']['id']
+	if message['text'] == 'New game':
+		(status, session['word'], session['current_word'], session['tries']) = new_game()
+		if not status:
+			reply(bot_id, 'Failed to start new game', session['id'])
 		else:
-			reply(bot_id, 'got nothing', session['id'])
-		for i in session:
-			print(i,session[i])
-		print()
+			session['game started'] = True
+			reply(bot_id, "Enter a character", session['id'])
+	elif message['text'] == 'Cancel' and session['game started']:
+		session.clear()
+	elif session['game started']:
+		(status, current_word) = guess(session['word'], session['current_word'], session['tries'], message['text'])
+		if status:
+			reply(bot_id, 'perfect \nCurrent word is'+''.join(current_word), session['id'])
+			reply(bot_id, 'Enter a character', session['id'])
 	else:
-		return "<form action = ''><input name = 'message' type = 'text' placeholder = 'input'></form>"
+		reply(bot_id, 'got nothing', session['id'])
+	for i in session:
+		print(i,session[i])
+	print()
 
 if __name__ == '__main__':
 	app.secret_key = 'Super Secret Key'
