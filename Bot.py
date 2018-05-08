7f275dcdc067f258f7bfbef735229ffdeb7ed68e
ekam walia <ekamwalia@gmail.com>
2018-05-08 21:29:39 +0530

    fixed helpers

--- Bot.py
+++ Bot.py
@@ -5,31 +5,34 @@ app = Flask(__name__)
 app.secret_key = 'Super Secret Key'
 bot_id = os.environ['BOT_ID']
 
+
 @app.route('/', methods=['POST'])
 def game():
-	req = request.get_json(silent = True, force = True)
-	message = req.get('message')
-	print()
-	session['id'] = message['from']['id']
-	if message['text'].upper() == 'NEW GAME':
-		(status, session['word'], session['current_word'], session['tries']) = new_game()
-		if not status:
-			reply(bot_id, 'Failed to start new game', session['id'])
-		else:
-			session['game started'] = True
-			reply(bot_id, "Enter a character", session['id'])
-	elif message['text'] == 'CANCEL' and session['game started']:
-		session.clear()
-	elif session['game started']:
-		(status, current_word) = guess(session['word'], session['current_word'], session['tries'], message['text'])
-		if status:
-			reply(bot_id, 'perfect \nCurrent word is'+''.join(current_word), session['id'])
-			reply(bot_id, 'Enter a character', session['id'])
-	else:
-		reply(bot_id, 'got nothing', session['id'])
-	for i in session:
-		print(i,session[i])
-	print()
+    req = request.get_json(silent=True, force=True)
+    message = req.get('message')
+    print()
+    session['id'] = message['from']['id']
+    if message['text'].upper() == 'NEW GAME':
+        (status, session['word'], session['current_word'], session['tries']) = new_game()
+        if not status:
+            reply(bot_id, 'Failed to start new game', session['id'])
+        else:
+            session['game started'] = True
+            reply(bot_id, "Enter a character", session['id'])
+    elif message['text'] == 'CANCEL' and session['game started']:
+        session.clear()
+    elif session['game started']:
+        (status, current_word) = guess(session['word'], session['current_word'], session['tries'], message['text'])
+        if status:
+            reply(bot_id, 'perfect \nCurrent word is'+''.join(current_word), session['id'])
+            reply(bot_id, 'Enter a character', session['id'])
+    else:
+        reply(bot_id, 'got nothing', session['id'])
+    for i in session:
+        print(i, session[i])
+    print()
+
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug=True)
+
--- helpers.py
+++ helpers.py
@@ -1,80 +1,87 @@
 import os
 import requests
 
+
 def get_new_word():
-	return ['A','B','C','D',' ','E','F','G','A']
+    return ['A', 'B', 'C', 'D', ' ', 'E', 'F', 'G', 'A']
+
 
 def new_game():
-	word = get_new_word()
-	if word == None:
-		return (False, None, None, None)
-	current_word = ['_' for i in range(len(word))]
-	for i in range(len(word)):
-		if not word[i].isalnum():
-			current_word[i] = word[i]
-	tries = 3
-	return (True, word, current_word, tries)
+    word = get_new_word()
+    if word == None:
+        return (False, None, None, None)
+    current_word = ['_' for i in range(len(word))]
+    for i in range(len(word)):
+        if not word[i].isalnum():
+            current_word[i] = word[i]
+    tries = 3
+    return (True, word, current_word, tries)
+
 
 def guess(word, current_word, tries, letter):
-	letter = letter.upper()
-	if letter not in word:
-		tries -= 1
-		return (False, current_word)
-	else:
-		for i in range(len(word)):
-			if word[i] == letter:
-				current_word[i] = letter
-	return (True, current_word)
+    letter = letter.upper()
+    if letter not in word:
+        tries -= 1
+        return (False, current_word)
+    else:
+        for i in range(len(word)):
+            if word[i] == letter:
+                current_word[i] = letter
+    return (True, current_word)
+
 
 def display(current_word):
-	os.system('clear')
-	print("\n\nCurrent Word = ", end = '')
-	for i in current_word:
-		print(i,end = '')
-	print()
+    os.system('clear')
+    print("\n\nCurrent Word = ", end='')
+    for i in current_word:
+        print(i, end='')
+    print()
+
 
 def get_letter():
-	return input('Enter a character to guess:').upper()
+    return input('Enter a character to guess:').upper()
+
 
 def if_won(word, current_word):
-	for i in range(len(word)):
-		if word[i] != current_word[i]:
-			return False
-	print("Yay! You've Won!!!")
-	return True
+    for i in range(len(word)):
+        if word[i] != current_word[i]:
+            return False
+    print("Yay! You've Won!!!")
+    return True
+
 
 def if_lost(tries):
-	if tries == 0:
-		print("Shucks, seems like you've lost")
-		return True
-	return False
+    if tries == 0:
+        print("Shucks, seems like you've lost")
+        return True
+    return False
+
 
 def reply(bot_id, message, chat_id):
-	url = 'https://api.telegram.org/bot' + str(bot_id) + '/sendMessage'
-	request = {'chat_id' : chat_id, 'text' : message}
-	response = requests.post(url, json = request)
-	print(request)
+    url = 'https://api.telegram.org/bot' + str(bot_id) + '/sendMessage'
+    request = {'chat_id': chat_id, 'text': message}
+    response = requests.post(url, json=request)
+    print(request)
+
 
 def __init__():
-	word = []
-	current_word = []
-	tries = []
-	(status, word, current_word, tries) = new_game(word, current_word, tries)
-	if not status:
-		print("Initialization failed")
-		return -1
-	done = False
-	display(current_word)
-	while not done:
-		letter = get_letter()
-		(result, current_word) = guess(word, current_word, tries, letter)
-		display(current_word)
-		if result:
-			print('Well done')
-		else:
-			print(letter + ' was not in the word')
-			print('remaining tries = ' + str(tries))
-		done = if_won(word, current_word) or if_lost(tries)
+    (status, word, current_word, tries) = new_game()
+    if not status:
+        print("Initialization failed")
+        return -1
+    done = False
+    display(current_word)
+    while not done:
+        letter = get_letter()
+        (result, current_word) = guess(word, current_word, tries, letter)
+        display(current_word)
+        if result:
+            print('Well done')
+        else:
+            print(letter + ' was not in the word')
+            print('remaining tries = ' + str(tries))
+        done = if_won(word, current_word) or if_lost(tries)
+
 
 if __name__ == '__main__':
-	__init__()
+    __init__()
